a1178859035011beda0525ae4dfc5fc93c1f7b53
goliakshay357 <goliakshay357@gmail.com>
2020-01-07 01:06:17 +0530

    Made first frase of Drawing

--- src/app/components/main-board/main-board.component.ts
+++ src/app/components/main-board/main-board.component.ts
@@ -14,22 +14,57 @@ export class MainBoardComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.p5_drawing()
+  }
+
+  drawing = [];
+
+  p5_drawing(){
+
+
+
 
     new p5(p => {
       let x = 100;
       let y = 100;
-
+  
       p.setup = () => {
         p.createCanvas(700, 410);
       };
-
+      
+      //the main draw function:
       p.draw = () => {
         p.background(0);
         p.fill(255);
-        p.rect(x, y, 50, 50);
+        
+        if(p.mouseIsPressed){
+          var point = {
+            x: p.mouseX,
+            y:p.mouseY
+          }
+
+          this.drawing.push(point)
+          console.log(this.drawing);
+        }
+
+        p.beginShape();
+        p.stroke(255);
+        p.strokeWeight(4);
+        p.noFill();
+
+        for(var i=0;i<this.drawing.length;i++){
+          p.vertex(this.drawing[i].x,this.drawing[i].y)
+        }
+        p.endShape();
+
+
+
+
+
+
       };
     }, this.el.nativeElement);
-
   }
-
 }
+
+
